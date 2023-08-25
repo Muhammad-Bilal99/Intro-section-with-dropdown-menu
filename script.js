ca725a0a63452c7be9329728389a35478f840917
Muhammad-Bilal99 <beastop975@gmail.com>
2023-08-25 17:21:55 +0500

    Almost done

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 const dropdown = document.querySelector('.dropdownn');
-const dropdownMenu = document.getElementsByClassName('dropdown-menu');
-const dropdownIcon = document.getElementsByClassName('dropdown-icon');
+const dropdown2 = document.querySelector('.dropdownn2');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownMenu2 = document.querySelector('.dropdown-menu2');
+const dropdownIcon = document.querySelector('.dropdown-icon');
+const dropdownIcon2 = document.querySelector('.dropdown-icon2');
 const hamburger = document.querySelector('.hamburger');
 const ul = document.querySelector('.ul');
+const links = document.querySelector('.nav-links')
+
 
 
   dropdown.addEventListener('click', (e) => {
-    e.preventDefault();
-    dropdownMenu.classList.toggle('active');
+    dropdownMenu.classList.toggle('active')
+    if (dropdownMenu.classList.contains('active')) {
+          dropdownIcon.src = 'images/icon-arrow-up.svg';
+          }else{
+          dropdownIcon.src = 'images/icon-arrow-down.svg';
+          };
+  })
+  dropdown2.addEventListener('click', (e) => {
+    dropdownMenu2.classList.toggle('active')
+    if (dropdownMenu2.classList.contains('active')) {
+          dropdownIcon2.src = 'images/icon-arrow-up.svg';
+          }else{
+          dropdownIcon2.src = 'images/icon-arrow-down.svg';
+          };
   })
 
-// dropdowns.forEach(dropdown => {
-//   dropdown.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     dropdownMenu.classList.toggle('active');
-//     if (dropdownMenu.classList.contains('active')) {
-//     dropdownIcon.src = 'images/icon-arrow-up.svg';
-//     }else{
-//     dropdownIcon.src = 'images/icon-arrow-down.svg';
-//     };
-//   });
-// })
 
 
 hamburger.addEventListener('click', () => {
-  ul.classList.toggle('mobile_nav');
+  links.classList.toggle('mobile_nav');
 });
+
+
 
 
 
